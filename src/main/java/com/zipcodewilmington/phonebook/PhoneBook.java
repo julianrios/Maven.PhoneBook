90fb80ff15c88d6d4dc003eae4e04f71463d6724
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-02-15 15:59:43 -0500

    added test code and main methods for phonebook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,78 @@
 package com.zipcodewilmington.phonebook;
 
+import apple.laf.JRSUIUtils;
+
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private TreeMap<String, String> phoneBook;
+//    private String name;
+//    private String phoneNumber;
+
+    public PhoneBook() {
+        this.phoneBook = new TreeMap<String, String>();
+    }
+
+    public void add(String name, String phoneNumber) {
+        phoneBook.put(name, phoneNumber);
+    }
+
+    public void addByNumber(String number) {
+        String name = "Default";
+        add(name, number);
+    }
+
+    public void remove(String name) {
+        phoneBook.remove(name);
+    }
+
+    public void removeByNumber(String number) {
+        String name = reverseLookup(number);
+
+        if(!(name.equals(null))) {
+            remove(name);
+        } else {
+            System.out.println("Error");
+        }
+
+    }
+
+
+    public void removeRecord(String name) {
+        remove(name);
+    }
+
+
+    public String lookup(String name) {
+        String number = phoneBook.get(name);
+        return number;
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        String name = "";
+        for (String searchingName : phoneBook.keySet()) {
+
+            String number = phoneBook.get(searchingName);
+            if(number.equals(phoneNumber)) {
+                     name = searchingName;
+                } else {
+                name = null;
+            }
+        }
+        return name;
+    }
+
+    public String display() {
+        String entry = "";
+        for (String name: phoneBook.keySet()) {
+            // search for the value
+            String number = phoneBook.get(name);
+            entry += String.format("%s %s\n", name,number);
+        }
+        return entry;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,158 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+//   private TreeMap<String, String> phoneBook;
+//
+//   @Before
+//   public void phonebookTest() {
+//      this.phoneBook = new PhoneBook();
+//   }
+
+
+   @Test
+   public void addTest() {
+      // given
+//      TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+      PhoneBook phoneBook = new PhoneBook();
+      String name = "Julian";
+      String phoneNumber = "123";
+      phoneBook.add(name, phoneNumber);
+      String expected = "123";
+
+      // when
+      String actual = phoneBook.lookup("Julian");
+
+      // then
+      Assert.assertEquals(expected, actual);
+   }
+
+   @Test
+   public void removeTest() {
+      // given
+//      TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+      PhoneBook phoneBook = new PhoneBook();
+      String name = "Julian";
+      String phoneNumber = "123";
+      phoneBook.add(name, phoneNumber);
+
+      // when
+      phoneBook.remove("Julian");
+      String actual = phoneBook.lookup("Julian");
+
+      // then
+      Assert.assertNull(actual);
+   }
+
+   @Test
+   public void lookupTest() {
+      // given
+//      TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+      PhoneBook phoneBook = new PhoneBook();
+      String name = "Julian";
+      String expectedPhoneNumber = "123";
+      phoneBook.add(name, expectedPhoneNumber);
+      phoneBook.lookup(name);
+
+      // when
+      String actual = phoneBook.lookup(name);
+
+      // then
+      Assert.assertEquals(expectedPhoneNumber, actual);
+   }
+
+   @Test
+   public void reverseLookupTest() {
+      // given
+      PhoneBook phoneBook = new PhoneBook();
+      String expectedName = "Julian";
+      String phoneNumber = "123";
+      phoneBook.add(expectedName, phoneNumber);
+
+
+      // when
+      String actualName = phoneBook.reverseLookup(phoneNumber);
+
+      // then
+      Assert.assertEquals(expectedName, actualName);
+
+   }
+
+   @Test
+   public void displayTest() {
+      // given
+//      TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+      PhoneBook phoneBook = new PhoneBook();
+//      String name = "Julian";
+//      String phoneNumber = "123";
+//      phoneBook.add(name, phoneNumber);
+      phoneBook.add("Zebra", "111-222-333");
+      phoneBook.add("Dog", "222-444-4444");
+      String expectedOutput = "Dog 222-444-4444\nZebra 111-222-333\n";
+
+      // when
+      String actual = phoneBook.display();
+
+      // then
+      Assert.assertEquals(expectedOutput, actual);
+   }
+
+
+   @Test
+   public void removeRecordTest() {
+      // given
+//      TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+      PhoneBook phoneBook = new PhoneBook();
+      String name = "Julian";
+      String phoneNumber = "123";
+      phoneBook.add(name, phoneNumber);
+
+      // when
+      phoneBook.removeRecord("Julian");
+      String actual = phoneBook.lookup("Julian");
+
+      // then
+      Assert.assertNull(actual);
+   }
+
+   @Test
+   public void removeByNumberTest() {
+      PhoneBook phoneBook = new PhoneBook();
+      String name = "Julian";
+      String phoneNumber = "123";
+      phoneBook.add(name, phoneNumber);
+
+      // when
+      phoneBook.removeByNumber("123");
+//      String actual = phoneBook.reverseLookup("123");
+      String actual = phoneBook.lookup("Julian");
+      System.out.println(actual);
+      // then
+      Assert.assertNull(actual);
+   }
+
+   @Test
+   public void addByNumberTest() {
+      // given
+//      TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+      PhoneBook phoneBook = new PhoneBook();
+      String phoneNumber = "123";
+      phoneBook.addByNumber(phoneNumber);
+      String expected = "Default";
+
+      // when
+      String actual = phoneBook.reverseLookup("123");
+
+      // then
+      Assert.assertEquals(expected, actual);
+   }
+
 }
